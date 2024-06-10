8357ec9bf08ebc37312afe3934a1e28a9592f021
SunFlow <sunflow.riven@gmail.com>
2024-06-10 22:30:04 +0200

    fixed spelling error

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ export function Hero() {
 		<div className='text-center text-lg'>
 			<h1 className='mb-8 text-center text-5xl'>Hello World</h1>
 			<p>This is a rustic starter template for Next.js</p>
-			<p>Typscript, Tailwind, Shadcn, Darkmode are all configured and ready to go</p>
+			<p>Typescript, Tailwind, Shadcn, Darkmode are all configured and ready to go</p>
 			<p>It also includes an improved implementation of the React Context-API</p>
 			<p>and a very simple Logger to easily enable/disable debugging</p>
 		</div>
